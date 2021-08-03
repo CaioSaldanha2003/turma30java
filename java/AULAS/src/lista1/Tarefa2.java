d64b218ae16b51297bb20bb93fcca3b28a238cec
Caio Saldanha <caiosaldanha2231@gmail.com>
2021-08-03 13:18:19 -0300

    Lista5 Ex2

---
+++ java/AULAS/src/lista1/Tarefa2.java
@@ -0,0 +1,19 @@
+package lista1;
+
+import java.util.Scanner;
+
+public class Tarefa2 {
+	public static void main(String[] args) {
+	Scanner ler = new Scanner(System.in);
+	int idadeDias,anos,meses;
+	
+	System.out.println("Mande sua idade em dias: ");
+	idadeDias = ler.nextInt();
+	
+	anos = (idadeDias)/365;
+	meses = ((idadeDias)%365)/30;
+	
+	System.out.println("Sua idade em anos é: "+anos+". Sua idade em meses é: "+meses+".");
+
+}
+}
