4694bec2a472cf9e3343c79d1db3ab0b9ab1b4d8
Caio Saldanha <caiosaldanha2231@gmail.com>
2021-08-03 13:08:46 -0300

    primeiro programa java

---
+++ java/AULAS/src/lista1/Tarefa1.java
@@ -0,0 +1,26 @@
+package lista1;
+
+import java.util.Scanner;
+
+public class Tarefa1 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int anos,meses,dias,idade;
+		
+		System.out.println("Digite sua idade em anos: ");
+		anos = ler.nextInt();
+		System.out.println("Meses remanescentes: ");
+		meses = ler.nextInt();
+		System.out.println("Dias remanescentes: ");
+		dias = ler.nextInt();
+		
+		anos = anos*365;
+		meses = meses*30;
+		idade = anos+meses+dias;
+		
+		System.out.println("Oi!!! Sua idade em dias é "+idade+".");
+
+	}
+
+}
