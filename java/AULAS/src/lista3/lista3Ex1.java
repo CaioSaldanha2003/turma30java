bfde7f5c55c4fbcb6b58a4854e7f9c776e115cc3
Caio Saldanha <caiosaldanha2231@gmail.com>
2021-08-04 21:03:30 -0300

    Projeto

--- java/AULAS/src/lista3/lista3Ex1.java
+++ java/AULAS/src/lista3/lista3Ex1.java
@@ -1,5 +1,14 @@
 package lista3;
 
 public class lista3Ex1 {
-
+	public static void main(String[] args) {
+		
+		for(int x = 1000; x <= 1999; x++) {
+			
+			if(x % 11 == 5) {
+				System.out.println(x);		
+			}
+		}
+				
+	}	
 }
--- java/AULAS/src/lista3/lista3Ex3.java
+++ java/AULAS/src/lista3/lista3Ex3.java
@@ -1,6 +1,6 @@
 package lista3;
 
-public class lista3Ex6 {
+public class lista3Ex3 {
 	public static void main(String[] args) {
 		
 	}
