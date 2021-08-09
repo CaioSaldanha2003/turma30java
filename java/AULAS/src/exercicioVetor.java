380857fa5cf3ece36faa1b6bf14151e9714e8d00
Caio Saldanha <caiosaldanha2231@gmail.com>
2021-08-08 23:18:02 -0300

    Projeto

--- java/AULAS/src/exercicioVetor.java
+++ java/AULAS/src/exercicioVetor.java
@@ -6,66 +6,24 @@ public class exercicioVetor {
 		/*Criar uma aplicação que consiga ler 5 nomes e cinco notas de uma determinada turma em VETOR.
 		A aplicação irá ler e calcular e exibir a média das notas da turma.
 		E em seguida será exibida a relação de nomes cuja nota é superior a esta média.*/
-		String[] alunos = {"Luffy","Zoro","Nami", "Usopp", "Sanji"};
-		int[] notas = {2, 3, 10, 7, 8};
+		String[] alunos = {"Larissa","Cecília","Laura", "Lays", "Mel"};
+		int[] notas = {2, 3, 9, 7, 8};
 		int acumulador = 0;
 		int media = 0;
 		
-						System.out.println("ALUNOS\tNOTAS");
+						System.out.println("ALUNAS:\tNOTAS:");
 					for(int x = 0; x < 5; x++) {
 				System.out.println(alunos[x]+"\t- ["+notas[x]+"]");
 			acumulador += notas[x];
 		media = (acumulador / notas.length);
 	}
-		System.out.printf("\nA média das notas é %d.\n",media);
+					
+		System.out.printf("\nA média das notas é %d.\n\n",media);
 		
-		for(int x = 0; x < 5; x++) {
+		System.out.println("FEEDBACK: ");
 		
-			System.out.println(alunos[x]+"\t- ["+notas[x]+"]");
-			
-			
-			if(notas[0] > media) {
-			System.out.println("\nEstá acima da média, parabéns!!!");
-			}
-			else if(notas[0] < media) {
-			System.out.println("\nEstá abaixo da média, precisa estudar mais");
-			}
-			
-			
-			else if(notas[1] > media) {
-			System.out.println("\nEstá acima da média, parabéns!!!");
-			}
-			else if(notas[1] < media) {
-			System.out.println("\nEstá abaixo da média, precisa estudar mais");
-			}
-			
-			
-			else if(notas[2] > media) {
-			System.out.println("\nEstá acima da média, parabéns!!!");
-			}
-			else if(notas[2] < media) {
-			System.out.println("\nEstá abaixo da média, precisa estudar mais");
-			}
-			
-			else if(notas[3] > media) {
-			System.out.println("\nEstá acima da média, parabéns!!!");
-			}
-			else if(notas[3] < media) {
-			System.out.println("\nEstá abaixo da média, precisa estudar mais");
-			}
-			
-			
-			else if(notas[4] > media) {
-			System.out.println("\nEstá acima da média, parabéns!!!");
-			}
-			else if(notas[4] < media) {
-			System.out.println("\nEstá abaixo da média, precisa estudar mais");
-			}
-			
-			else {
-				System.out.println("sei lá man");
+				for(int x = 0; x < 5; x++) {
+					System.out.println(alunos[x]+"\t- ["+notas[x]+"]"+(notas[x] > media ? "\t- Parabéns, você é uma nerd!!!" : "\t- Cê precisa estudar amiguxa. . ."));
 			}
 		}
 	}
-
-}
