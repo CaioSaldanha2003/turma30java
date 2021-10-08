69bca571d720b60a440af391cc2dd12847dd1072
Caio Saldanha <caiosaldanha2231@gmail.com>
2021-10-07 23:12:03 -0300

    seila

--- spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ spring/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -1,7 +1,9 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.generation.blogPessoal.model.Tema;
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
 import org.generation.blogPessoal.repository.PostagemRepository;
@@ -29,16 +31,23 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioRepository repository;
 	
-	@PostMapping("/logar")
-	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
-		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
-				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	@GetMapping
+	public ResponseEntity<List<Usuario>> GetAllUsuario(){
+		return ResponseEntity.ok(repository.findAll());
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Usuario> GetById(@PathVariable long id){
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	
 	
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
